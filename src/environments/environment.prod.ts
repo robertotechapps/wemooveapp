815489bf377cd8fde42117bd13a59050efaeb689
robertotechapps <157500138+robertotechapps@users.noreply.github.com>
2024-06-08 15:54:18 -0300

    add .

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  appShellConfig: {
+    debug: false,
+    networkDelay: 500
+  },
+  url: 'https://back.wemoove.app' //http://ec2-18-205-185-151.compute-1.amazonaws.com'
+};
